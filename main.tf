e8248e6660cd7d2f57babde305fc3299f99eabbd
Sybil Melton <8355229+ngnetworkpro@users.noreply.github.com>
2026-05-21 12:32:42 -0400

    feat: add playbookcandle_worker cloudflare_worker resource to main.tf (#7)

--- main.tf
+++ main.tf
@@ -35,3 +35,24 @@ resource "cloudflare_workers_custom_domain" "sybilsedge_www" {
   zone_id     = var.cloudflare_zone_id
   environment = "production"
 }
+
+resource "cloudflare_worker" "playbookcandle_worker" {
+  account_id = var.cloudflare_account_id
+  logpush    = false
+  name       = "playbookcandle"
+  observability = {
+    enabled            = true
+    head_sampling_rate = 1
+    logs = {
+      enabled            = true
+      head_sampling_rate = 1
+      invocation_logs    = true
+    }
+  }
+  subdomain = {
+    enabled          = true
+    previews_enabled = true
+  }
+  tags           = []
+  tail_consumers = []
+}
